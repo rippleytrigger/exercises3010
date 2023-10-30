62b952b7ed056387fd4771b12d64ef46a3c15608
Rippley <rippleytrigger@gmail.com>
2023-10-30 17:46:45 +0100

    Exercises

--- exercise2.js
+++ exercise2.js
@@ -1,8 +1,12 @@
-let a, b, c, d = 5;
+let a = b = 10;
+let c = d = 15;
 
-if(a + b > c + d)
+sum1 = a + b;
+sum2 = c + d;
+
+if(sum1 > sum2)
     console.log("A y B son mayores que C y D");
-else if (a + b < c + d)
+else if (sum1 < sum2)
     console.log("C y D son mayores que A y B");
 else 
     console.log("Ninguno de LOS DOS SON MAYORES")
